dd158c48ef84c8be350a4d2ba355866cbfe4cb28
coderbyducnam13 <97455760+coderbyducnam13@users.noreply.github.com>
2022-03-24 12:25:42 +0700

    Add files via upload

---
+++ songuyen.py
@@ -0,0 +1,8 @@
+so_nguyen = int(input("Nhập vào số nguyên"))
+float_so_nguyen = float(so_nguyen)
+if float_so_nguyen%2==0:
+    print("số chẵn")
+elif float_so_nguyen%2==1:
+    print("số lẻ")
+else:
+    print('không phải số tự nhiên')
