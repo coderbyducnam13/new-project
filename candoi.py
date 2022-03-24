937800116554cad609c2ee74ceb7610c978c716f
coderbyducnam13 <97455760+coderbyducnam13@users.noreply.github.com>
2022-03-24 13:05:06 +0700

    Add files via upload

---
+++ candoi.py
@@ -0,0 +1,19 @@
+chieu_cao = int("Hãy nhập vào chiều cao của bạn")
+can_nang = int("Hãy nhập vào cân nặng của bạn")
+bmi = can_nang/(chieu_cao**2)
+if bmi < 16:
+	print("Gầy cấp độ III")
+elif 16 <= bmi < 17:
+	print("Gầy cấp độ II")
+elif 17<= bmi < 18.5:
+	print("Gầy cấp độ I")
+elif 18.5 <= bmi < 25:
+	print("Bình thường")
+elif 25 <= bmi < 30:
+	print("Thừa cân")
+elif 30 <= bmi < 35:
+	print("Béo phì cấp độ I")
+elif 35 <= bmi < 40:
+	print("Béo phì cấp độ II")
+else:
+	print("Béo phì cấp độ III")
