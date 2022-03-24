d45e7e18bd1b09cec4959b9746ad1415f739765e
coderbyducnam13 <97455760+coderbyducnam13@users.noreply.github.com>
2022-03-24 12:27:51 +0700

    Add files via upload

---
+++ ngaytrongtuan.py
@@ -0,0 +1,17 @@
+ngay = int(input("Give me a number (form 1 to 7): "))
+if ngay == 1:
+	print("Monday")
+elif ngay == 2:
+	print("Tuesday")
+elif ngay == 3:
+	print("Wednesday")
+elif ngay == 4:
+	print("Thursday")
+elif ngay == 5:
+	print("Friday")
+elif ngay == 6:
+	print("Saturday")
+elif ngay == 7:
+	print("Sunday")
+else:
+	print("Error, out of range")
